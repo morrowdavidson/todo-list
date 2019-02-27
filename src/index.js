247e8360dc6236152c9d30c2894d0f771344c563
Morrow Davidson <morrow.davidson@gmail.com>
2019-02-27 11:51:39 -0800

    Finish installs

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './scss/app.scss';
+import 'bootstrap';
+
+$('div').scrollspy({ target: '#list-nav' });
+
+// // Import bootstrap plugins individually as needed:
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/dropdown';

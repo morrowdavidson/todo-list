1f155cbe0971f40700bdee94eb1611c6fa08bb47
Morrow Davidson <morrow.davidson@gmail.com>
2019-03-03 02:40:56 -0800

    Make nav full js no html template

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './scss/app.scss';
 import 'bootstrap';
 import { } from './pill-nav';
 
-
 // // Import bootstrap plugins individually as needed:
 // import 'bootstrap/js/dist/util';
 // import 'bootstrap/js/dist/dropdown';
--- src/pill-nav.js
+++ src/pill-nav.js
@@ -1,17 +1,103 @@
-let navLinks = Array.from(document.getElementsByClassName("nav-link"));
+const navOptions = {
+    navTitles: [ 
+        "Today",
+        "This week",
+        "All tasks",
+    ]
+};
 
-navLinks.forEach(function(element){
-    element.addEventListener('click', setActive);
-});
+const snakeCase = function(string) {
+    // wtf case
+    string = string.replace(/\.\./g, " ");
+  
+    return string
+      .trim()
+      .toLowerCase()
+      .replace(/[,\?\.]/g, "")
+      .replace(/\-/g, " ")
+      .split(" ")
+      .join("-");
+};
 
-function setActive(){
+function setAttributes(el, attrs) {
+    for(var key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+}
+
+const createNav = () => document.createElement('nav');
+const createUl = () => document.createElement('ul');
+const createLi = () => document.createElement('li');
+const createLink = () => document.createElement('a');
+
+let navAction = (navLink) => {
+    setActive(navLink);
+    let navLinkId = navLink.getAttribute('id');
+    showContent(navLinkId);
+};
+
+let setActive = (navLink) => {
     clearActive();
-    this.classList.add('active');
-    console.log(this.getAttribute('class'));
+    navLink.classList.add('active');
+};
+
+let clearActive = () => {
+    const navLinks = Array.from(document.getElementsByClassName('nav-link'));
+    navLinks.forEach(function(navListItem){
+        navListItem.classList.remove('active');
+    });
+};
+
+function showContent(){
+
 }
 
-function clearActive(){
-    navLinks.forEach(function(element){
-        element.classList.remove('active');
+const navList = [];
+
+function constructNavList () {
+
+    navOptions.navTitles.forEach(linkTxt => {
+        let linkId = snakeCase(linkTxt) + '-nav'; 
+        let li = createLi();
+        li.setAttribute('class','nav-item');
+        let link = createLink();
+        setAttributes(link, {
+            'id'    :   linkId,
+            'class' :   "nav-link",
+            'href'  :    '#' + linkId,     
+        });
+        link.innerHTML = linkTxt;
+        
+        link.addEventListener('click', function(){
+            navAction(this);
+        });
+
+        li.appendChild(link);
+
+        navList.push(li);
     });
+    return navList;
+} 
+
+function constructNav() {
+    const page = document.getElementById('page');
+
+    constructNavList ();
+    let nav = createNav();
+    setAttributes(nav, {'id': 'pill-nav'});
+
+    let ul = createUl();
+    setAttributes(ul, {
+        'id'    :   'list-nav', 
+        'class' :   'nav nav-pills nav-fill'
+    });
+
+    navList.forEach(navItem => {
+        ul.appendChild(navItem);
+    }); 
+    nav.appendChild(ul);
+    page.appendChild(nav);
 }
+
+constructNav();
+setActive(navList[0].firstChild);
